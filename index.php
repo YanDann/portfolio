f65cf658d5480da87ce3d12cf20fb654920cf74a
Julien Michon <mcn.julien@gmail.com>
2023-07-04 23:53:06 +0200

    add first section content + fix some css

--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <?php require 'partials/header.php'; ?>
 
 <main>
-    <p>Salut, moi c'est </p>
-    <h1>Julien Michon.</h1>
-    <h2>Designer devenu développeur;</h2>
-    <p class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, quasi nesciunt! Quaerat id, mollitia veritatis distinctio vitae, dolor perferendis voluptates culpa explicabo tempora ipsum at impedit consequatur? Odit, laboriosam soluta!</p>
+    <section class="me">
+        <p>Salut, moi c'est </p>
+        <h1>Julien Michon.</h1>
+        <h2>Designer devenu développeur;</h2>
+        <p class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, quasi nesciunt! Quaerat id, mollitia veritatis distinctio vitae, dolor perferendis voluptates culpa explicabo tempora ipsum at impedit consequatur? Odit, laboriosam soluta!</p>
+    </section>
+
+    <section class="about">
+        <div class="titleSection">
+            <h2><span>01.</span> A propos</h2>
+            <div class="lineParag"></div>
+        </div>
+
+        <div class="sectionContent">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum magni natus, facilis sunt autem mollitia! Illo suscipit quis quam tempore quia! Dolorem doloremque eos eveniet quia sunt, qui repellat molestiae.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum magni natus, facilis sunt autem mollitia! Illo suscipit quis quam tempore quia! Dolorem doloremque eos eveniet quia sunt, qui repellat molestiae.</p>
+            <p>Voici les technologies que j'utilise dans mes projets :</p>
+            <div class="techno">
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>C#</li>
+                </ul>
+                <ul>
+                    <li>PHP</li>
+                    <li>Java</li>
+                    <li>Laravel</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+        <div class="photo">
+
+        </div>
+    </section>
 </main>
 
 <?php require 'partials/footer.php'; ?>
