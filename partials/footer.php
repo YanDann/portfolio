6176ef5cb50ed053d6ca6e5ca2e5fe7d32176132
Julien Michon <mcn.julien@gmail.com>
2023-07-04 22:03:59 +0200

    portoflio v1 - header & footer base

---
+++ partials/footer.php
@@ -0,0 +1,19 @@
+<div class="leftLine">
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <div class="decoLine"></div>
+    </ul>
+</div>
+
+<div class="rightLine">
+    <ul>
+        <a href="mailto:mcn.julien@gmail.com">mcn.julien@gmail.com</a>
+        <div class="decoLine"></div>
+    </ul>
+</div>
+
+</body>
+
+</html>
---
+++ partials/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Julien Michon</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <ul>
+                <li><span>01.</span>A propos</li>
+                <li><span>02.</span>Experiences</li>
+                <li><span>03.</span>Contact</li>
+                <a href="" target="_blank">CV</a>
+            </ul>
+        </nav>
+    </header>
