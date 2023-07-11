4f120671401105f06d05835a51c3b73b3f032162
Julien Michon <mcn.julien@gmail.com>
2023-07-12 00:01:39 +0200

    add contact form

--- config/functions.php
+++ config/functions.php
@@ -12,30 +12,9 @@ function validEmail(string $email): bool{
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
-/**
- * Permet de valider des valeurs dans un tableau
- */
-function validArray(array $data, array $valid): bool{
-    foreach ($data as $item){
-        if (!in_array($item, $valid)){
-            return false;
-        }
-    }
-    return true;
-}
-
-/**
- * Permet d'afficher cehcked si une valeur est dans un tableau
- */
-function checked(string $value, array $array): string{
-    return in_array($value, $array) ? 'checked' : '';
-}
-
 function showErrors(string $value, array $errors) {
     if (!empty($errors)) { ?>
-        <div class="alert alert-danger">
-            <p class="m-0"><?= $errors[$value]; ?></p>
-        </div>
+        <p class="invalid-text"><?= $errors[$value]; ?></p>
     <?php } 
 }
 
--- partials/05-contact.php
+++ partials/05-contact.php
@@ -1,11 +1,82 @@
-<section class="experiences flex" data-aos="fade-left" data-aos-duration="2000">
-     <div class="titleSection flex">
-         <h2><span>05.</span> Me contacter</h2>
-         <div class="lineParag"></div>
-     </div>
+<?php
 
-     <div class="sectionContent">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam accusantium tempora perferendis molestias fuga quas at doloribus optio, nobis amet.</p>
-     </div>
- </section>
- 
+$email = $_POST['email'] ?? null;
+if (isset($email)) {
+    $name = strstr($email, '@', true);
+}
+
+$message = $_POST['message'] ?? null;
+$carac = 15;
+
+$subject = $_POST['subject'] ?? null;
+$subjectTab = [
+    'Proposition de stage',
+    'Proposition d\'emploi',
+    'Demande de projet',
+];
+
+$errors = [];
+
+
+if (!empty($_POST)) {
+    if (empty($email)) {
+        $errors['email'] = "L'email est obligatoire.";
+        $validEmail = "invalid";
+    } else if (!validEmail($email)) {
+        $errors['email'] = "L'email est invalide";
+        $validEmail = "invalid";
+    }
+
+    if (!isset($subject)) {
+        $errors['subject'] = "Vous devez choisir un sujet.";
+        $validSubject = "invalid";
+    }
+
+    if (strlen($message) < $carac) {
+        $errors['message'] = "Le message doit faire au moins $carac carractères.";
+        $validMessage = "invalid";
+    }
+
+    if (empty($errors)) {
+        $success = "Bonjour $name, votre " . strtolower($subject) . " à bien été envoyée et sera prise en compte dans les plus bref délais.";
+    }
+}
+
+?>
+
+<section class="experiences flex" data-aos="fade-left" data-aos-duration="2000">
+    <div class="titleSection flex">
+        <h2><span>05.</span> Me contacter</h2>
+        <div class="lineParag"></div>
+    </div>
+
+    <?php if (isset($success)) { ?>
+        <span><?= $success; ?></span>
+    <?php } ?>
+
+
+    <div class="contactContent">
+        <form method="post">
+            <label for="email">Email</label>
+            <?php showErrors('email', $errors); ?>
+            <input class="<?= $validEmail; ?>" type="text" id="email" name="email" placeholder="name@gmail.com" value="<?= $email; ?>">
+
+            <label for="subject">Sujet</label>
+            <?php showErrors('subject', $errors); ?>
+            <select class="<?= $validSubject; ?>" name="subject" id="subject">
+                <option selected disabled>Choisissez un sujet</option>
+                <?php for ($i = 0; $i < count($subjectTab); $i++) { ?>
+                    <option <?= $subject == $subjectTab[$i] ? 'selected' : '' ?> value="<?= $subjectTab[$i]; ?>">
+                        <?= $subjectTab[$i]; ?>
+                    </option>
+                <?php } ?>
+            </select>
+
+            <label for="message">Message</label>
+            <?php showErrors('message', $errors); ?>
+            <textarea class="<?= $validMessage; ?>" name="message" id="message" cols="30" rows="10" placeholder="Ecrivez votre message"><?= $message; ?></textarea>
+
+            <button type="submit" class="submitButton">Envoyer</button>
+        </form>
+    </div>
+</section>
