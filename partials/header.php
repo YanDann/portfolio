0ad4b52c6e5915ed94459d7935bd2119e0a73bf1
Julien Michon <mcn.julien@gmail.com>
2023-08-14 13:17:48 +0200

    fix redirection (no more reloading)

--- partials/header.php
+++ partials/header.php
@@ -24,11 +24,11 @@ $url = 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
                 <a href="index.php"><img src="./img/logo.png" alt=""></a>
             </div>
             <ul>
-                <li><span>01.</span><a href=<?= strpos($url, 'index') == true ? '#about' : 'index.php';?>>A propos</a></li>
-                <li><span>02.</span><a href=<?= strpos($url, 'index') == true ? '#xp' : 'index.php';?>>Experiences</a></li>
-                <li><span>03.</span><a href=<?= strpos($url, 'index') == true ? '#jobs' : 'index.php';?>>Créations</a></li>
-                <li><span>04.</span><a href=<?= strpos($url, 'index') == true ? '#design' : 'index.php';?>>Designs</a></li>
-                <li><span>05.</span><a href=<?= strpos($url, 'index') == true ? '#contact' : 'index.php';?>>Contact</a></li>
+                <li><span>01.</span><a href=<?= strpos($url, 'julienmichon') == true ? '#about' : 'index.php';?>>A propos</a></li>
+                <li><span>02.</span><a href=<?= strpos($url, 'julienmichon') == true ? '#xp' : 'index.php';?>>Experiences</a></li>
+                <li><span>03.</span><a href=<?= strpos($url, 'julienmichon') == true ? '#jobs' : 'index.php';?>>Créations</a></li>
+                <li><span>04.</span><a href=<?= strpos($url, 'julienmichon') == true ? '#design' : 'index.php';?>>Designs</a></li>
+                <li><span>05.</span><a href=<?= strpos($url, 'julienmichon') == true ? '#contact' : 'index.php';?>>Contact</a></li>
                 <a class="cv" href="__DIR__/../img/CV_MICHON_Julien.pdf" target="_blank">CV</a>
             </ul>
         </nav>
